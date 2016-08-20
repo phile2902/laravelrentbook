39002b25dd600e564873bcfcdaf180f76abd2f2f
PhiLe <phi2902@yahoo.com>
2016-08-20 17:30:31 +0300

    Create new model and migration of book and category

---
+++ database/migrations/2016_08_20_142345_create_books_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->index()->unsigned();
+            $table->integer('category_id')->index()->unsigned();
+            $table->string('name');
+            $table->text('info');
+            $table->text('imgLink');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('books');
+    }
+}
